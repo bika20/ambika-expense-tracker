a9bb062ccb98f22b08f4b0cd5a3ddecd010e73af
Sanskar Singh <163184569+MeSanskarSingh@users.noreply.github.com>
2025-08-03 23:12:46 +0530

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   <>
     <GlobalProvider>
       <div className="min-h-screen flex flex-col items-center justify-center">
-        <h1 className='text-[40px] mb-3 mt-3 madimi-one-regular text-[#203A06]'>EXPEN$E TRACKER</h1>
+        <h1 className='text-[40px] mb-3 mt-3 madimi-one-regular text-[#203A06] font-bold'>EXPEN$E TRACKER</h1>
         <div className="w-[40%] bg-white p-4 rounded-lg shadow-lg">
           <div className="w-full flex flex-col justify-center items-center">
             <Balance />
